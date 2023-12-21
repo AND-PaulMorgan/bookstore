21dbf036966a38469178752a73f93730e452ff8a
Paul Morgan <paul.morgan@and.digital>
2023-12-21 10:04:20 +0000

    Add some validation hints to the CreateBookDTO

--- bookstoreapi/src/main/java/digital/and/bookstoreapi/dto/request/v1/CreateBookDto.java
+++ bookstoreapi/src/main/java/digital/and/bookstoreapi/dto/request/v1/CreateBookDto.java
@@ -1,7 +1,26 @@
 package digital.and.bookstoreapi.dto.request.v1;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
-public record CreateBookDto(String isbn, String title, String description, String[] tags, String authors, LocalDate releaseDate) {
+public record CreateBookDto(
+        @NotBlank(message = "ISBN cannot be blank")
+        @Size(min = 10, max = 18, message ="ISBN must be between 10 and 18 characters")
+        String isbn,
+
+        @NotBlank(message = "Title cannot be blank")
+        @Size(min=1, max=255, message = "Title must be between 1 and 255 characters")
+        String title,
+
+        @Size(min=0, max=255, message = "Description is optional but must not be longer than 255 characters")
+        String description,
+        String[] tags,
+
+        @NotBlank(message = "You must list at least one author")
+        @Size(min=1, max=255, message = "Author(s) are limited to 255 characters")
+        String authors,
+        LocalDate releaseDate) {
 
 }
